55a9c69a4288d977b473a6653f63ae2df9439a9a
Alan Buscaglia <alanbuscaglia@gmail.com>
2019-02-21 13:37:45 -0300

    clase 6 Reactive Form

--- src/app/main/main/main.component.ts
+++ src/app/main/main/main.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -7,13 +8,48 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  @Input() otraLogica = false;
   idFromParams: number;
+  myForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.idFromParams = params['id'];
     });
+
+    this.myForm = this.formBuilder.group({
+      email: ['', [Validators.maxLength(10), IncluyeVocal]],
+      password: [''],
+      condicional: ['soy el condicional']
+    });
+
+    if (this.otraLogica) {
+      this.myForm.get('email').setValidators([Validators.maxLength(10)]);
+      this.myForm.get('email').updateValueAndValidity();
+
+      this.myForm.get('condicional').disable();
+    }
   }
+
+  submitForm() {
+    console.log(this.myForm.value);
+  }
+}
+
+function IncluyeVocal(control: AbstractControl) {
+  if (!tieneVocal(control.value)) {
+    return { incluyeVocal: true };
+  } else {
+    return null;
+  }
+}
+
+function tieneVocal(ourString: string) {
+  if (/[aeiou]/.exec(ourString)) {
+    return true;
+  }
+
+  return false;
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserInformationComponent } from './user-information/user-information.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UserInformationComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [UserInformationComponent]
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [UserInformationComponent, ReactiveFormsModule]
 })
-export class SharedModule { }
+export class SharedModule {}
